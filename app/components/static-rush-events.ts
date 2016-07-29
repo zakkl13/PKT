3afc825fa5279b7ec03b85e58e025eb143935ede
Zakk Lefkowits <Zakk Lefkowits>
2016-07-28 21:34:12 -0400

    style improvements

--- app/components/static-rush-events.ts
+++ app/components/static-rush-events.ts
@@ -3,7 +3,7 @@ import {RushEvent} from './rush_event'
 export const EVENTS: RushEvent[] = [
   {day: "Monday, September 5th", title: 'Cookout on the Quad', img: './static/campus.jpg',
               body: 'Cooking out at Pritchard quad, bring your friends, your dog, and your parents! Come eat the free food.'},
-  {day: "Wednesday, September 7th", title: 'Hokie House', img: './static/fraternity.jpg', body: 'come chill'},
+  {day: "Wednesday, September 7th", title: 'Hokie House', img: './static/hohouse.jpg', body: 'come chill'},
   {day: "Friday, September 9th", title: 'Dodgeball', img: './static/fraternity.jpg', body: 'throw balls'},
   {day: "Tuesday, September 13th", title: 'Bowling at Frank\'s Cinebowl', img: './static/fraternity.jpg', body: 'come bowl'},
   {day: "Thursday, September 15th", title: 'Poker & Cigar Night', img: './static/fraternity.jpg', body: 'play poker, smoke cigars'},
