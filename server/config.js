dc1b8a4d0ed3f96a8f97da3577be8c22b32fa1b8
Zakk Lefkowits <zakkl13@vt.edu>
2016-08-17 15:01:32 -0400

    cmon gitignore!

--- server/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.isProd = process.env.NODE_ENV === 'production';
-exports.port = process.env.PORT || 3000;
-exports.mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/pktDB';
-exports.secret = process.env.SECRET || '7%ZD6Lr03g41';
-//# sourceMappingURL=config.js.map
--- server/models/rush_event.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-class RushEvent {
-}
-exports.RushEvent = RushEvent;
-//# sourceMappingURL=rush_event.js.map
