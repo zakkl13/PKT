d8c541ad9cac94874c7515239e9c536f328a7e16
Zakk Lefkowits <zakkl13@vt.edu>
2016-08-17 15:02:46 -0400

    get em all

--- app/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var root_component_1 = require('./root.component');
-platform_browser_dynamic_1.bootstrap(root_component_1.RootComponent);
-//# sourceMappingURL=main.js.map
