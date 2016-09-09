77d0a5df4c3e7693bc4e357706639445593e72fb
Zakk Lefkowits <zakkl13@vt.edu>
2016-09-09 11:23:30 -0400

    fb open graph optimizations

--- app/components/philanthropy.component.ts
+++ app/components/philanthropy.component.ts
@@ -11,8 +11,8 @@ export class PhilanthropyComponent {
     chair_loaded: boolean = false;
 
     panels = [
-        ["About", "Phitauberfest is a one-day philanthropy event designed to promote the same fun and inclusive spirit of the SeriousFun programming that it directly supports."],
-        ["Grand Prize", "At the end of the event, the organization with the most points will receive a $200 donation to their respective philanthropic mission or charitable organization"]
+        ["About", "Phitauberfest is a one-day philanthropy fund-raising event, open to any organization at Virginia Tech, designed to promote the same fun and inclusive spirit of the SeriousFun programming that it directly supports. All funds raised will be donated directly to Serious Fun Children's Network."],
+        ["Grand Prize", "The organization with the most points at the end of the event will receive a $200 donation to their respective philanthropic mission or charitable organization"]
              ];
 
     private togglePhilChair() {
